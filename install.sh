385d5981c954b1f5c8b6abf4f6f66c2ec0d2a49f
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-11-16 07:55:45 -0800

    fixed installation order

--- install.sh
+++ install.sh
@@ -16,14 +16,14 @@ if [[ $OSTYPE == 'darwin'* ]] ; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
         brew update
+    fi
     cd ~/.dots/apple
     brew bundle
-    fi
 fi
 cd
 cd ~/.dots
 stow -vt ~ git tmux vim zsh #alacritty kitty
-stow -vt ~/.config/kitty kitty
+#stow -vt ~/.config/kitty kitty
 stow -vt ~/.config starship
 echo "done!"
 
