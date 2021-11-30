1e1d00302c9c15d6d1921f48d81a7756c077dd3f
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-11-30 13:16:25 -0800

    small changes, made installation script universal

--- install.sh
+++ install.sh
@@ -9,18 +9,6 @@ cd .zsh_plugins
 if [ ! -d ~/.zsh_plugins/zsh-syntax-highlighting ]; then 
     git clone git@github.com:zsh-users/zsh-syntax-highlighting.git
 fi
-cd
-if [[ $OSTYPE == 'darwin'* ]] ; then
-    which -s brew
-    if [[ $? != 0 ]] ; then
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-        brew update
-    fi
-    cd ~/.dots/apple
-    brew bundle
-fi
-cd
 cd ~/.dots
 stow -vt ~ git tmux vim zsh #alacritty kitty
 #stow -vt ~/.config/kitty kitty
