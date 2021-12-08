f98e9107c5a874bb3a395d50f0372906623a14f4
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2021-12-07 21:14:25 -0800

    forgot what I changed tbh

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 set -eu
-cd
-if [ ! -d ~/.zsh_plugins ]; then
-    mkdir .zsh_plugins
-fi
-cd .zsh_plugins
-if [ ! -d ~/.zsh_plugins/zsh-syntax-highlighting ]; then 
-    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git
-fi
-cd ~/.dots
+
 stow -vt ~ git tmux vim zsh #alacritty kitty
-#stow -vt ~/.config/kitty kitty
 stow -vt ~/.config starship
+
+if [ ! -d ~/.zsh_plugins ]; then
+    mkdir ~/.zsh_plugins
+fi
+if [ ! -d ~/.zsh_plugins/zsh-syntax-highlighting ]; then 
+    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh_plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/.zsh_plugins/gitstatus
+fi
 echo "done!"
 
