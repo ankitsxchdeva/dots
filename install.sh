ec5eac4511ca795bf91e59ca15fb8a94178d65fc
Ankit Sachdeva <ankitsachdeva001@gmail.com>
2025-01-01 14:48:09 -0800

    updated brewfile, updated installation, added ghostty stuff

--- install.sh
+++ install.sh
@@ -2,15 +2,31 @@
 
 set -eu
 
-stow -vt ~ git tmux vim zsh #alacritty kitty
-stow -vt ~/.config starship
+stow_home() {
+    stow --restow -vt ~ git tmux vim zsh
+}
 
-if [ ! -d ~/.zsh_plugins ]; then
-    mkdir ~/.zsh_plugins
-fi
-if [ ! -d ~/.zsh_plugins/zsh-syntax-highlighting ]; then 
-    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ~/.zsh_plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/gitstatus.git ~/.zsh_plugins/gitstatus
-fi
-echo "done!"
+stow_config() {
+    stow --restow -vt ~/.config starship ghostty
+}
+
+install_plugins() {
+    local zsh_plugins_dir=~/.zsh_plugins
+    if [ ! -d "$zsh_plugins_dir" ]; then
+        mkdir "$zsh_plugins_dir"
+    fi
+    if [ ! -d "$zsh_plugins_dir/zsh-syntax-highlighting" ]; then
+        git clone git@github.com:zsh-users/zsh-syntax-highlighting.git "$zsh_plugins_dir/zsh-syntax-highlighting"
+        git clone --depth=1 https://github.com/romkatv/gitstatus.git "$zsh_plugins_dir/gitstatus"
+    fi
+}
+
+main() {
+    stow_home
+    stow_config
+    install_plugins
+    echo "Installation complete!"
+}
+
+main
 
